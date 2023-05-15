a00c962aefa3c24727ddd78b4d090bc0d3a53379
s4ddo <ahmadramadan407@gmail.com>
2023-05-15 14:38:11 +0200

    THE CHAT WORKS NOW

--- Loginscript.js
+++ Loginscript.js
@@ -2,50 +2,6 @@ var objPeople = [
 {
 username: "saraii",
 password: "ngekekman123456789"   //yousef #1
-},
-{
-username: "dappal",   //daffal #2
-password: "duk"
-},
-{
-username: "lolifighter", //mutia #3
-password: "flatty"
-},
-{
-username: "ahmad",    //me #4
-password: "ayahmama3235"
-},
-{
-username: "butan",  //sultan #5
-password: "elwafi"
-},
-{
-username: "abduh28",  //abduh #6
-password: "1234567"
-},
-{
-username: "Faizah",  //Faizah #7
-password: "idkidc"
-},
-{
-username: "Chrono",  //Hendrick #8
-password: "animeforlifeu"
-},
-{
-username: "mary222",  //Maritza #9
-password: "dressesed"
-},
-{
-username: "rowan",  //Rowan #10
-password: "rowan123"
-},
-{
-username: "Oxygen",  //shadowjake #11
-password: "iamnotarobot"
-},
-{
-username: "adibdiw22",  //Rowan #10
-password: "dibdiw22"
 }
 ]
 
